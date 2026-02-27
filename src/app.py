976734db6022e129acb0183e1faf44c9b5831634
Jose Davila <jose.dmyt@gmail.com>
2026-02-26 20:44:56 -0800

    adding scatter plot

--- src/app.py
+++ src/app.py
@@ -1,4 +1,9 @@
 from shiny import App, ui
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data/spotify_songs.csv")
+numeric_columns = df.select_dtypes(include="number").columns.tolist()
 
 app_ui = ui.page_fillable(
     ui.panel_title("SPOTIFIND"),
@@ -9,12 +14,19 @@ app_ui = ui.page_fillable(
             ui.card(ui.card_header("Results tables"), full_screen=True),
         ),
         ui.layout_columns(
-            ui.value_box("Drop down", "X, Y drop down for scatter plot"),
+            ui.value_box(
+                "Drop down",
+                ui.input_select("x_var", "Select X-axis", choices=numeric_columns),
+                ui.input_select("y_var", "Select Y-axis", choices=numeric_columns),
+            ),
             ui.card(ui.card_header("Top genre")),
             fill=False,
         ),
         ui.layout_columns(
-            ui.card(ui.card_header("Scatter plot"), full_screen=True),
+            ui.card(
+                ui.card_header("Scatter plot"), \
+                ui.output_plot("scatter_plot")
+                full_screen=True),
             ui.card(ui.card_header("Song search"), full_screen=True),
             col_widths=[6, 6],
         ),
@@ -22,6 +34,19 @@ app_ui = ui.page_fillable(
 )
 
 def server(input, output, session):
-    pass
+   
+    @output
+    @render.plot
+    def scatter_plot():
+        x = input.x_var()
+        y = input.y_var()
+
+        fig, ax = plt.subplots()
+        ax.scatter(df[x], df[y])
+        ax.set_xlabel(x)
+        ax.set_ylabel(y)
+        ax.set_title(f"{x} vs {y}")
+
+        return fig
 
 app = App(app_ui, server)
